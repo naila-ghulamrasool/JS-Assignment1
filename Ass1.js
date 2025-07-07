5fad654b38d49d7de1be814866335c3496f69da0
naila-ghulamrasool <nailaghulam99@gmail.com>
2025-07-07 23:11:05 +0500

    Add files via upload

---
+++ Ass1.js
@@ -0,0 +1,6 @@
+alert("JavaScript Alert\n\nError! please enter a valid password");
+document.writeln("JavaScript Alert <br><br> Error! please enter a valid password");
+alert("JavaScript Alert\n\n Welcome to JS Land...\n Happy Coding!");
+alert("JavaScript Alert\n\n Welcome to JS Land");
+alert("JavaScript Alert\n\n Happy Coding!\n prevent this page from creating additional dialogs");
+console.log("JavaScript Alert\n\n Hello i can run JS through my web browser's console");
